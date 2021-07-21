609b7b97e6862b76a57ab3010830d3890f722112
CichonJakub <48649767+CichonJakub@users.noreply.github.com>
2021-07-21 14:17:07 +0200

    unshelving_end of sec3

---
+++ src/Section3/Ifstatemats.java
@@ -0,0 +1,10 @@
+package Section3;
+
+public class Ifstatemats {
+    public static void main(String[] args) {
+        boolean isAlien = true;
+
+        if(isAlien)
+            System.out.println("he is an Alien!!! RUN!");
+    }
+}
