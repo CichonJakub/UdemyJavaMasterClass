9e7cc632caa4595562919c4ab951d9c62c146728
CichonJakub <48649767+CichonJakub@users.noreply.github.com>
2021-08-02 13:10:41 +0200

    exercises - section 4

--- src/Section4/Exercise4.java
+++ src/Section4/Exercise4.java
@@ -2,8 +2,21 @@ package Section4;
 
 public class Exercise4 {
 
-
-
-
-
+    //leap year == 366 days in this year
+    public static boolean isLeapYear(int year){
+        if(year < 1 || year > 9999)
+            return false;
+        else {
+            if(year % 4 == 0){
+                if(year % 100 == 0){
+                    if(year % 400 == 0) {
+                        return true;
+                    } else return false;
+                } else return true;
+            } else return false;
+        }
+    }
 }
+
+
+
